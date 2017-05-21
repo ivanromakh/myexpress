5233b14044add251beb324138c5891b2a5a527e1
Ivan Romakh <Ivan Romakh>
2017-05-21 11:46:01 +0300

    'use_express-session'

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var expressSession = require('express-session');
+var expressValidator = require('express-validator');
 var app = express();
+
+app.use(expressSession({
+	secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}));
+
 
 app.get('/', function(req, res){
   res.send('hello world');
